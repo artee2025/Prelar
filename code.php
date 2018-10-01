69f5548fa344b23427f563d5ed0ed874a940b789
Artee <artee2025@gmail.com>
2018-10-01 22:12:35 +0700

    Edit

--- code.php
+++ code.php
@@ -1,8 +1,13 @@
 <?php
-$p=array('ru'=>'https://site.com/ru', 'uk'=>'https://site.com/ru', 'en1'=>'https://site.com/en');
+$p = array('ru' => 'https://site.com/ru', 'uk' => 'https://site.com/ru', 'en' => 'https://site.com/en');
+$l = 'https://site.com/en';
 $hal = strtolower($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-$l = 'en'; $min = 10000;
-foreach($p as $k => $v){
-	$t = strpos($hal, $k);
-	if ((!is_bool($t))&($t<$min)) {$min = $t; $l = $v;}}
-header($l);
+$min = 10000;
+foreach ($p as $k => $v) {
+    $t = strpos($hal, $k);
+    if ((!is_bool($t)) & ($t < $min)) {
+        $min = $t;
+        $l = $v;}
+}
+header('Location: ' . $l);
+die();
